bfd241e6935f5b70965dadfec401e6a5c2cc73a2
Mauliklm10 <askmaulik2000@gmail.com>
2021-05-17 15:58:27 -0400

    Minor Changes

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ import os, random, sys, time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
+browser = webdriver.Chrome('chromedriver.exe')
 
+browser.get('https://www.linkedin.com/uas/login')
 
+file = open('config.txt')
+lines = file.readlines()
+username = lines[0]
+password = lines[1]
+
+elementID = browser.find_element_by_id('username')
+elementID.send_keys(username)
+
+elementID = browser.find_element_by_id('password')
+elementID.send_keys(password)
+
+elementID.submit()
+
+browser.get('https://www.linkedin.com/groups/12508395/members/')
 
 
 
